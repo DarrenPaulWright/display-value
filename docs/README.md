54c4abe9ad33a9c43cc87c1ca882d1cd57a11c56
DarrenPaulWright <darren@darrenpaulwright.com>
2024-11-22 21:06:19 -0800

    Switching to typedoc for docs

---
+++ docs/README.md
@@ -0,0 +1,76 @@
+**display-value** • **Docs**
+
+***
+
+# display-value
+
+## Functions
+
+### default()
+
+```ts
+function default(value: unknown, settings: ISettings): string
+```
+
+Designed for use in test messages, displayValue takes a javascript value and returns a human-readable string representation of that value.
+
+Notes:
+- Finite numbers are passed through number.toLocaleString()
+  - -0 is rendered as -0
+  - 1300 is rendered as 1,300 (depending on locale)
+- Strings are wrapped in double quotes
+- Arrays and Objects are passed through a function similar to JSON.stringify, but values are individually run through displayValue
+- Array-like values such as arguments are handled like Arrays
+- Object-like values such as ClientRect and DOMRect are handled like Objects
+- Constructors will return the constructor's name
+- Instances of non-native constructors:
+  - will return the result of .toString() if other than '[object Object]'
+  - otherwise returns '[object Name]' where Name is the constructor's name
+
+#### Parameters
+
+| Parameter | Type |
+| ------ | ------ |
+| `value` | `unknown` |
+| `settings` | [`ISettings`](-internal-.md#isettings) |
+
+#### Returns
+
+`string`
+
+#### Example
+
+``` javascript
+import displayValue from 'display-value';
+
+displayValue(-0); // '-0'
+displayValue(1300000); // '1,300,000'
+displayValue('foo'); // '"foo"'
+displayValue({ x: 1 }); // '{ "x": 1 }'
+
+displayValue(() => {}); // '() => {…}'
+displayValue(function(param) {}); // 'ƒ (param) {…}'
+displayValue(function name() {}); // 'ƒ name() {…}'
+
+displayValue(Symbol()); // 'Symbol()'
+displayValue(Symbol('name')); // 'Symbol(name)'
+
+displayValue(new CustomClass()); // '[object CustomClass]'
+
+displayValue([{x: 1}, {x: 2000}], { beautify: true });
+// '[{
+//     "x": 1
+// }, {
+//     "x": 2,000
+// }]'
+```
+
+#### Defined in
+
+[index.ts:67](https://github.com/DarrenPaulWright/display-value/blob/main/index.ts#L67)
+
+## Modules
+
+| Module | Description |
+| ------ | ------ |
+| [\<internal\>](-internal-.md) | - |
--- index.ts
+++ index.ts
@@ -22,13 +22,13 @@ interface ISettings {
 }
 
 /**
- *  Designed for use in test messages, displayValue takes a javascript value and returns a human-readable string representation of that value.
+ * Designed for use in test messages, displayValue takes a javascript value and returns a human-readable string representation of that value.
  *
  * Notes:
- * - finite numbers are passed through number.toLocaleString()
+ * - Finite numbers are passed through number.toLocaleString()
  *   - -0 is rendered as -0
  *   - 1300 is rendered as 1,300 (depending on locale)
- * - strings are wrapped in double quotes
+ * - Strings are wrapped in double quotes
  * - Arrays and Objects are passed through a function similar to JSON.stringify, but values are individually run through displayValue
  * - Array-like values such as arguments are handled like Arrays
  * - Object-like values such as ClientRect and DOMRect are handled like Objects
@@ -45,7 +45,7 @@ interface ISettings {
  * displayValue(-0); // '-0'
  * displayValue(1300000); // '1,300,000'
  * displayValue('foo'); // '"foo"'
- * displayValue({x: 1}); // '{"x": 1}'
+ * displayValue({ x: 1 }); // '{ "x": 1 }'
  *
  * displayValue(() => {}); // '() => {…}'
  * displayValue(function(param) {}); // 'ƒ (param) {…}'
@@ -56,25 +56,13 @@ interface ISettings {
  *
  * displayValue(new CustomClass()); // '[object CustomClass]'
  *
- * displayValue([{x: 1}, {x: 2000}], {beautify: true});
- * // '[
- * //     {
- * //         "x": 1
- * //     }, {
- * //         "x": 2,000
- * //     }
- * // ]'
+ * displayValue([{x: 1}, {x: 2000}], { beautify: true });
+ * // '[{
+ * //     "x": 1
+ * // }, {
+ * //     "x": 2,000
+ * // }]'
  * ```
- *
- * @function displayValue
- *
- * @param {*} value
- * @param {object} [settings]
- * @param {boolean} [settings.beautify=false] - If true and value is an Array or Object then the output is rendered in multiple lines with indentation
- * @param {boolean} [settings.preferJson=true] - If true then keys and strings are wrapped in double quotes, similar to JSON.stringify.
- * @param {boolean} [settings.preferSingleQuote=false] - If true then strings will be wrapped in single quotes. Only applicable if preferJson is false.
- *
- * @returns {string}
  */
 const displayValue = (value: unknown, settings: ISettings = {}): string => {
 	return stringifyAny(value, 0, {
